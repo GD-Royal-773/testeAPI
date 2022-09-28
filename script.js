082646314c425d5b1b9a7b471dfc749da82cb379
GD-Royal-773 <105356928+GD-Royal-773@users.noreply.github.com>
2022-09-28 08:30:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let espaco = document.querySelector('#nomec');
+let space = document.querySelector('#nomeb');
+let botao = document.querySelector('.button');
+
+function enviar() {
+        alert('Obrigado,os seus dados foram encaminhados com sucesso,breve seram analizados pelos DEVs');
+}   
+botao.addEventListener('onclick', enviar);
